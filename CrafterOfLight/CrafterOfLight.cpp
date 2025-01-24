e91e20211690d1b5cf648ea1e153893aee964997
CChiverton <75337792+CChiverton@users.noreply.github.com>
2025-01-24 20:43:56 +0000

    Example functions
    A demonstration of how to interact with UI elements and provide functions to the buttons.

--- CrafterOfLight/CrafterOfLight.cpp
+++ CrafterOfLight/CrafterOfLight.cpp
@@ -5,7 +5,40 @@ CrafterOfLight::CrafterOfLight(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
+    connect(ui.pushButton_bruteCraft, &QPushButton::clicked, this, &CrafterOfLight::BruteCraft);
+    connect(ui.pushButton_smartCraft, &QPushButton::clicked, this, &CrafterOfLight::SmartCraft);
 }
 
 CrafterOfLight::~CrafterOfLight()
 {}
+
+void CrafterOfLight::BruteCraft() {
+    DeleteMacros();
+    ui.spinBox->setValue(15);
+}
+
+void CrafterOfLight::SmartCraft() {
+    DeleteMacros();
+    ui.gridLayout_macroOutput->addWidget(new QPushButton("1"), 0, 0);
+    QLineEdit* option = new QLineEdit("This is an example");                // worry about memory leak: QObjects should be deleted on program end
+                                                                            // but during runtime this could build up
+    option->setReadOnly(true);
+    ui.gridLayout_macroOutput->addWidget(option, 0, 1);
+}
+
+void CrafterOfLight::DeleteMacros() {
+    int row = 0;
+    while (ui.gridLayout_macroOutput->count()) {
+        QWidget* widgetButton = ui.gridLayout_macroOutput->itemAtPosition(row,0)->widget();
+        QWidget* widgetText = ui.gridLayout_macroOutput->itemAtPosition(row, 1)->widget();
+        if (widgetButton) {
+            ui.gridLayout_macroOutput->removeWidget(widgetButton);
+            delete widgetButton;
+        }
+        if (widgetText) {
+            ui.gridLayout_macroOutput->removeWidget(widgetText);
+            delete widgetText;
+        }
+        ++row;
+    }
+}
--- CrafterOfLight/CrafterOfLight.h
+++ CrafterOfLight/CrafterOfLight.h
@@ -11,6 +11,12 @@ public:
     CrafterOfLight(QWidget *parent = nullptr);
     ~CrafterOfLight();
 
+private slots:
+    void BruteCraft();
+    void SmartCraft();
+
 private:
     Ui::CrafterOfLightClass ui;
+
+    void DeleteMacros();
 };
