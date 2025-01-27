67755fb76411dee13e7acd47c269e848f3a01e8f
CChiverton <75337792+CChiverton@users.noreply.github.com>
2025-01-27 04:58:55 +0000

    Intermediary skills solution
    Added skills that will be used and their information to be used within the system.
    Added some buttons for each skill.

---
+++ CrafterOfLight/Skills.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "Skills.h"
+
+namespace Skills {
+
+	std::string GetSkillName(SkillName skillName) {
+		switch (skillName) {
+		case SkillName::BASICSYNTHESIS:
+			return "Basic Synthesis";
+		case SkillName::CAREFULSYNTHESIS:
+			return "Careful Synthesis";
+		case SkillName::PRUDENTSYNTHESIS:
+			return "Prudent Synthesis";
+		case SkillName::GROUNDWORK:
+			return "Groundwork";
+		case SkillName::MUSCLEMEMORY:
+			return "Muscle Memory";
+		case SkillName::BASICTOUCH:
+			return "Basic Touch";
+		case SkillName::STANDARDTOUCH:
+			return "Standard Touch";
+		case SkillName::ADVANCEDTOUCH:
+			return "Advanced Touch";
+		case SkillName::BYREGOTSBLESSING:
+			return "Byregot's Blessing";
+		case SkillName::PRUDENTTOUCH:
+			return "Prudent Touch";
+		case SkillName::PREPARATORYTOUCH:
+			return "Preparatory Touch";
+		case SkillName::REFLECT:
+			return "Reflect";
+		case SkillName::TRAINEDFINESSE:
+			return "Trained Finesse";
+		case SkillName::REFINEDTOUCH:
+			return "Refined Touch";
+		case SkillName::WASTENOTI:
+			return "Waste Not I";
+		case SkillName::WASTENOTII:
+			return "Waste Not II";
+		case SkillName::GREATSTRIDES:
+			return "Great Strides";
+		case SkillName::INNOVATION:
+			return "Innovation";
+		case SkillName::VENERATION:
+			return "Veneration";
+		case SkillName::FINALAPPRAISAL:
+			return "Final Appraisal";
+		case SkillName::MASTERSMEND:
+			return "Master's Mend";
+		case SkillName::MANIPULATION:
+			return "Manipulation";
+		case SkillName::IMMACULATEMEND:
+			return "Immaculate Mend";
+		case SkillName::TRAINEDPERFECTION:
+			return "Trained Perfection";
+		case SkillName::OBSERVE:
+			return "Observe";
+		case SkillName::DELICATESYNTHESIS:
+			return "Delicate Synthesis";
+		case SkillName::NONE:
+			return "None";
+		default:
+			return "Skill not found";
+		}
+		return "";
+	}
+
+	const std::array<SkillInformation, 26> SkillArray = { {			 /* CP	DURABILITY	EFFICIENCY	TIME*/
+		{SkillName::BASICSYNTHESIS,		SkillType::SYNTHESIS,			0,		10,		120,		3	},
+		{SkillName::CAREFULSYNTHESIS,	SkillType::SYNTHESIS,			7,		10,		180,		3	},
+		{SkillName::PRUDENTSYNTHESIS,	SkillType::SYNTHESIS,			18,		10,		180,		3	},
+		{SkillName::GROUNDWORK,			SkillType::SYNTHESIS,			18,		20,		360,		3	},
+		{SkillName::MUSCLEMEMORY,		SkillType::SYNTHESIS,			6,		10,		300,		3	},
+		{SkillName::BASICTOUCH,			SkillType::TOUCH,				18,		10,		120,		3	},
+		{SkillName::STANDARDTOUCH,		SkillType::TOUCH,				32,		10,		125,		3	},
+		{SkillName::ADVANCEDTOUCH,		SkillType::TOUCH,				46,		10,		150,		3	},
+		{SkillName::BYREGOTSBLESSING,	SkillType::TOUCH,				24,		10,		100,		3	},
+		{SkillName::PRUDENTTOUCH,		SkillType::TOUCH,				25,		10,		100,		3	},
+		{SkillName::PREPARATORYTOUCH,	SkillType::TOUCH,				40,		20,		200,		3	},
+		{SkillName::REFLECT,			SkillType::TOUCH,				6,		10,		300,		3	},
+		{SkillName::TRAINEDFINESSE,		SkillType::TOUCH,				32,		0,		100,		3	},
+		{SkillName::REFINEDTOUCH,		SkillType::TOUCH,				24,		10,		100,		3	},
+		{SkillName::WASTENOTI,			SkillType::BUFF,				56,		0,		120,		2	},
+		{SkillName::WASTENOTII,			SkillType::BUFF,				98,		0,		120,		2	},
+		{SkillName::GREATSTRIDES,		SkillType::BUFF,				32,		0,		120,		2	},
+		{SkillName::INNOVATION,			SkillType::BUFF,				18,		0,		120,		2	},
+		{SkillName::VENERATION,			SkillType::BUFF,				18,		0,		120,		2	},
+		{SkillName::FINALAPPRAISAL,		SkillType::BUFF,				1,		0,		120,		2	},
+		{SkillName::MASTERSMEND,		SkillType::REPAIR,				88,		-30,	120,		3	},
+		{SkillName::MANIPULATION,		SkillType::REPAIR,				96,		0,		120,		2	},
+		{SkillName::IMMACULATEMEND,		SkillType::REPAIR,				112,	-255,	120,		3	},
+		{SkillName::TRAINEDPERFECTION,	SkillType::OTHER,				0,	0	,		0,			3	},
+		{SkillName::OBSERVE,			SkillType::OTHER,				7,	0	,		0,			3	},
+		{SkillName::DELICATESYNTHESIS,	SkillType::OTHER,				32,		10,		150,		3	}
+	} };
+}
---
+++ CrafterOfLight/Skills.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <string>
+#include <array>
+/*
+Skills added here are usable regardless of item condition or specialisation
+*/
+namespace Skills {
+
+	const enum class SkillName {
+		/* Synthesis skills */
+		BASICSYNTHESIS = 0,
+		CAREFULSYNTHESIS,
+		PRUDENTSYNTHESIS,
+		GROUNDWORK,
+		MUSCLEMEMORY,
+
+		/* Touch skills */
+		BASICTOUCH,
+		STANDARDTOUCH,
+		ADVANCEDTOUCH,
+		BYREGOTSBLESSING,
+		PRUDENTTOUCH,
+		PREPARATORYTOUCH,
+		REFLECT,
+		TRAINEDFINESSE,
+		REFINEDTOUCH,
+
+		/* Buff skills */
+		WASTENOTI,
+		WASTENOTII,
+		GREATSTRIDES,
+		INNOVATION,
+		VENERATION,
+		FINALAPPRAISAL,
+
+		/* Repair skills */
+		MASTERSMEND,
+		MANIPULATION,
+		IMMACULATEMEND,
+
+		/* Other skills */
+		TRAINEDPERFECTION,
+		OBSERVE,
+		DELICATESYNTHESIS,
+
+		NONE
+	};
+
+	const enum class SkillType {
+		SYNTHESIS = 0,
+		TOUCH,
+		BUFF,
+		REPAIR,
+		OTHER,
+		NONE
+	};
+
+	const enum class SkillType;
+
+	std::string GetSkillName(SkillName skillName);
+	
+	const struct SkillInformation {
+		SkillName	name;
+		SkillType	type;
+		uint8_t		costCP;
+		int16_t		costDurability;
+		uint8_t		efficiency;
+		uint8_t		castTime;
+	};
+
+	extern const std::array<SkillInformation, 26> SkillArray;
+}
